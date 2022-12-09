87158f5f05a1af0cb7fc0844e4223575e379dc12
HugohGil <74474977+HugohGil@users.noreply.github.com>
2022-12-09 19:40:31 +0000

    timer working - simple

--- app/src/main/java/pt/isec/am_tp/SinglePlayerActivity.kt
+++ app/src/main/java/pt/isec/am_tp/SinglePlayerActivity.kt
@@ -1,15 +1,40 @@
 package pt.isec.am_tp
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import pt.isec.am_tp.databinding.ActivitySingleplayerBinding
+import kotlin.Long
+import kotlin.getValue
+
 
 class SinglePlayerActivity : AppCompatActivity() {
+    val timerModel : TimerViewModel by viewModels()
     private lateinit var binding: ActivitySingleplayerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySingleplayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        findViewById<TextView>(R.id.txtTimer)
+            .text = "${timerModel.timer}"
+        startTimer()
+    }
+
+    private fun startTimer() {
+        object : CountDownTimer((timerModel.timer*1000).toLong(), 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                findViewById<TextView>(R.id.txtTimer)
+                    .text = "${timerModel.timer}"
+                timerModel.timer--
+            }
+
+            override fun onFinish() {
+                findViewById<TextView>(R.id.txtTimer)
+                    .text = "bingbong"
+            }
+        }.start()
     }
 
 }
---
+++ app/src/main/java/pt/isec/am_tp/TimerViewModel.kt
@@ -0,0 +1,9 @@
+package pt.isec.am_tp
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TimerViewModel : ViewModel() {
+    var timer = 50
+}
