5628fc24b806c407982d2d0f559b6d5511ed8817
HugohGil <74474977+HugohGil@users.noreply.github.com>
2022-12-10 02:49:34 +0000

    data appearing in the board

--- app/src/main/java/pt/isec/am_tp/SinglePlayerActivity.kt
+++ app/src/main/java/pt/isec/am_tp/SinglePlayerActivity.kt
@@ -8,26 +8,57 @@ import androidx.appcompat.app.AppCompatActivity
 import pt.isec.am_tp.databinding.ActivitySingleplayerBinding
 import kotlin.Long
 import kotlin.getValue
+import kotlin.random.Random
 
 
 class SinglePlayerActivity : AppCompatActivity() {
-    val timerModel : TimerViewModel by viewModels()
+    val singlePlayerViewModel : SinglePlayerViewModel by viewModels()
     private lateinit var binding: ActivitySingleplayerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySingleplayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
         findViewById<TextView>(R.id.txtTimer)
-            .text = "${timerModel.timer}"
+            .text = "${singlePlayerViewModel.timer}"
+
+        singlePlayerViewModel.operations.add("+")
+        singlePlayerViewModel.operations.add("-")
+
+        generateBoard()
         startTimer()
     }
 
+    private fun generateBoard() {
+        val idsAr = arrayOf(R.id.btnAr0, R.id.btnAr1,R.id.btnAr2,R.id.btnAr3,R.id.btnAr4,R.id.btnAr5,
+            R.id.btnAr6,R.id.btnAr7,R.id.btnAr8,R.id.btnAr9,R.id.btnAr10,R.id.btnAr11)
+        val idsNum = arrayOf(R.id.btnNum0, R.id.btnNum1,R.id.btnNum2,R.id.btnNum3,R.id.btnNum4,R.id.btnNum5,
+            R.id.btnNum6,R.id.btnNum7,R.id.btnNum8)
+
+
+
+        for(i in 0..11){
+            val randomIndexAr = Random.nextInt(singlePlayerViewModel.operations.size)
+            val idAr = idsAr.get(i)
+            findViewById<TextView>(idAr)
+                .text = "${singlePlayerViewModel.operations.get(randomIndexAr)}"
+        }
+
+        for(j in 0..8){
+            val randomNum = Random.nextInt(1,singlePlayerViewModel.maxNumber)
+            val idNum = idsNum.get(j)
+            findViewById<TextView>(idNum)
+                .text = "${randomNum}"
+        }
+
+
+    }
+
     private fun startTimer() {
-        object : CountDownTimer((timerModel.timer*1000).toLong(), 1000) {
+        object : CountDownTimer((singlePlayerViewModel.timer*1000).toLong(), 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 findViewById<TextView>(R.id.txtTimer)
-                    .text = "${timerModel.timer}"
-                timerModel.timer--
+                    .text = "${singlePlayerViewModel.timer}"
+                singlePlayerViewModel.timer--
             }
 
             override fun onFinish() {
--- app/src/main/java/pt/isec/am_tp/TimerViewModel.kt
+++
@@ -1,9 +0,0 @@
-package pt.isec.am_tp
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class TimerViewModel : ViewModel() {
-    var timer = 50
-}
