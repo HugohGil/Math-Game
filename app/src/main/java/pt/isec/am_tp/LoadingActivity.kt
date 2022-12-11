3d797a330696ee761651ad56c13bc831f595fe9f
HugohGil <74474977+HugohGil@users.noreply.github.com>
2022-12-11 19:41:18 +0000

    app has levels missing endgame and a few details + timer visual bug

--- app/src/main/java/pt/isec/am_tp/LoadingActivity.kt
+++ app/src/main/java/pt/isec/am_tp/LoadingActivity.kt
@@ -1,25 +1,70 @@
 package pt.isec.am_tp
 
-import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.Query
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import pt.isec.am_tp.databinding.ActivityLoadingBinding
-import pt.isec.am_tp.databinding.ActivityTop5Binding
 
 class LoadingActivity : AppCompatActivity() {
+    companion object {
+        private const val POINTS_KEY = "points"
+        private const val LEVEL_KEY = "level"
+
+        fun getIntent(
+            context: Context,
+            points: Int,
+            level: Int,
+        ): Intent {
+            val intent = Intent(context, LoadingActivity::class.java)
+            intent.putExtra(POINTS_KEY, points)
+            intent.putExtra(LEVEL_KEY, level)
+            return intent
+        }
+    }
     private lateinit var binding: ActivityLoadingBinding
+    private var points: Int = 0
+    private var level: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoadingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        points = intent.getIntExtra(POINTS_KEY, 0)
+        level = intent.getIntExtra(LEVEL_KEY, 0)
+        level++
+        findViewById<TextView>(R.id.txtPoints)
+            .text = "$points"
+        findViewById<TextView>(R.id.txtDifficulty)
+            .text = "$level"
 
+        when(level){
+            2 -> {
+                findViewById<TextView>(R.id.txtTime)
+                    .text = "60"
+                findViewById<TextView>(R.id.txtExpression)
+                    .text = "10"
+                findViewById<TextView>(R.id.txtValue)
+                    .text = "1 - 19"
+                findViewById<TextView>(R.id.txtOperator)
+                    .text = "+ - x ÷"
+                findViewById<TextView>(R.id.txtBonus)
+                    .text = "5"
+            }
+            3-> {
+                findViewById<TextView>(R.id.txtTime)
+                    .text = "30"
+                findViewById<TextView>(R.id.txtExpression)
+                    .text = "15"
+                findViewById<TextView>(R.id.txtValue)
+                    .text = "1 - 99"
+                findViewById<TextView>(R.id.txtOperator)
+                    .text = "+ - x ÷"
+                findViewById<TextView>(R.id.txtBonus)
+                    .text = "10"
+            }
+        }
 
         startTimer()
     }
@@ -35,7 +80,7 @@ class LoadingActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                val intent = Intent(context, SinglePlayerActivity::class.java)
+                val intent = SinglePlayerActivity.getIntent(context, points, level)
                 startActivity(intent)
             }
         }.start()
--- app/src/main/java/pt/isec/am_tp/SinglePlayerViewModel.kt
+++ app/src/main/java/pt/isec/am_tp/SinglePlayerViewModel.kt
@@ -3,8 +3,11 @@ package pt.isec.am_tp
 import androidx.lifecycle.ViewModel
 
 class SinglePlayerViewModel : ViewModel() {
-    var timer = 60
+    var timerLimit = 90
     var operations: MutableList<String> = ArrayList()
     var maxNumber = 10
+    var expressions = 2
     var points = 0
+    var level = 1
+    var bonus = 0
 }
